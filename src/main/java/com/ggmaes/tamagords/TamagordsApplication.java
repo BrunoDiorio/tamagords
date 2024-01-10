17f794bd878eed29007dcbec7886db5db874c37e
bdiorio <b.pipinho@hotmail.com>
2024-01-09 23:48:50 -0300

    first commit

---
+++ src/main/java/com/ggmaes/tamagords/TamagordsApplication.java
@@ -0,0 +1,13 @@
+package com.ggmaes.tamagords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamagordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamagordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggmaes/tamagords/TamagordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggmaes.tamagords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamagordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
